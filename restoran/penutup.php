41ad1a24cb2015b3c01b1f2f692effd07f8cffcc
satrioandika <115439602+satrioandika@users.noreply.github.com>
2022-12-25 10:08:08 +0700

    Add files via upload

---
+++ restoran/penutup.php
@@ -0,0 +1,66 @@
+<?php
+$data = file_get_contents('penutup.json');
+$menu = json_decode($data, true);
+
+$menu = $menu["menu"];
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>McD</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="img/logo_mcd.png" width="50">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="index.php">All Menu</a>
+                    <a class="nav-link" href="sarapan.php">Sarapan Pagi</a>
+                    <a class="nav-link" href="ayam.php">Ayam</a>
+                    <a class="nav-link" href="ikan.php">Ikan</a>
+                    <a class="nav-link" href="minuman.php">Minuman</a>
+                    <a class="nav-link" href="penutup.php">Makanan Penutup</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="container">
+        <div class="row mt-3">
+            <div class="col">
+                <h1>Makanan Penutup</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <?php foreach ($menu as $row) : ?>
+            <div class="col-md-4">
+                <div class="card mb-3">
+                <img src="img/menu/<?= $row["gambar"]; ?>" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $row["nama"]; ?></h5>
+                        <p class="card-text"><?= $row["deskripsi"]; ?></p>
+                        <h5 class="card-title"><?= $row["harga"]; ?></h5>
+                        <a href="#" class="btn btn-primary">Pesan Sekarang</a>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  </body>
+</html>
